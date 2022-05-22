715135910056cc974949e2a3fc8b3cda12ecfa77
Benjamin <benjaminrowe0@gmail.com>
2022-05-22 17:20:37 +1000

    inital project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavgationBar from './components/NavgationBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <NavgationBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavgationBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavgationBar() {
+  return (
+    <nav>
+        <li>
+            Home
+        </li>
+        <li>
+            About
+        </li>
+        <li>
+            Project
+        </li>
+    </nav>
+  )
+}
+
+export default NavgationBar
