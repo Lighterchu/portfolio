9f91256823d50d2b14f96c8e983195973da2db69
Benjamin <benjaminrowe0@gmail.com>
2022-05-31 09:43:49 +1000

    added fetching to api, to bring down projects that I have

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Cards({title, info, points,image}){
+function Cards({title, info, points,image, color, changePos}){
     let mainPoints
 
    
@@ -10,13 +11,44 @@ function Cards({title, info, points,image}){
     }
     
     return (
-      <div className="cards-container">
-        <h1>{title}</h1>
+      <Wrapper changePos={changePos}>
+        <Title color={color}>
+          {title}
+        </Title>
         {image ? <img alt="loading"/> : null}
         <p>{info}</p>
        {points ? mainPoints : ""}
-      </div>
+      </Wrapper>
     )
 }
 
 export default Cards
+
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color:${({color}) => color ? color : 'white'};
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  width: 30%;
+  
+
+  color:rgb(233, 233, 233);
+  background-color: rgba(255, 64, 0, 0.633);
+
+  border-radius: 25px;
+  border: 2px solid #f1f6ea;
+
+  text-align:center;
+  align-items: center;
+
+  font-family: 'Times New Roman', Times, serif;
+  font-size:20px;
+
+  margin-left:${({changePos}) => changePos ? "2%" : '34%'};
+  margin-top:2%;
+  margin-bottom:2% ;
+`;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import Cards from '../components/Cards';
 
+
 function Projects() {
+
+  const [title, SetTitle] = useState('')
+  const [description, SetDescription] = useState('')
+
+  let DataFromRepo = () => {
+    
+   
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Lighterchu/repos")
+    .then(res => res.json())
+    .then(data => {
+      SetTitle(data[1].name)
+      SetDescription(data[1].description)
+    })
+  },[])
+
+
   return (
     <div className="project-container">
-       <Cards title="Project one" image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title={title} changePos={true} image={false} info={description} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
+       <Cards title="Project one" changePos={true} image={true} info={"Im a boot camp student as well a self taught programmer, I am very passion about the work I put into my projects, bringing my dreams to life"} />
     </div>
   )
 }
