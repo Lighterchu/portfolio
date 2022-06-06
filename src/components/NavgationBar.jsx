5ae3789b260e7d52ee8d96ffe3cfb580579887db
Benjamin <benjaminrowe0@gmail.com>
2022-06-06 21:34:29 +1000

    retry

--- src/components/NavgationBar.jsx
+++ src/components/NavgationBar.jsx
@@ -13,13 +13,13 @@ function NavgationBar() {
   <Container>
   <Navbar.Brand href="/">My Portfoilo</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="about">About</Nav.Link>
-      <Nav.Link href="resume">Resume</Nav.Link>
-      <Nav.Link href="projects">Projects</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="About">About</Nav.Link>
+        <Nav.Link href="Resume">Resume</Nav.Link>
+        <Nav.Link href="Projects">Projects</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Container>
 </Navbar>
   
