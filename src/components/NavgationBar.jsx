6738619356d593de8ff4701db7a8b96f59f2ae39
Benjamin <benjaminrowe0@gmail.com>
2022-06-06 22:21:41 +1000

    adding the files to main index html

--- src/components/NavgationBar.jsx
+++ src/components/NavgationBar.jsx
@@ -3,19 +3,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar,Nav,Container} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 
+import About from './pages/About'
+import Home from './pages/Home'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
 
 function NavgationBar() {
   
   return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-      <Navbar.Brand href="/home.index.html">My Portfoilo</Navbar.Brand>
+      <Navbar.Brand href="/home" as={<Home/>}>My Portfoilo</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/about.index.html">About</Nav.Link>
-            <Nav.Link href="/resume.index.html">Resume</Nav.Link>
-            <Nav.Link href="/projects.index.html">Projects</Nav.Link>
+            <Nav.Link href="/about" as={<About/>}>About</Nav.Link>
+            <Nav.Link href="/resume" as={<Resume/>}>Resume</Nav.Link>
+            <Nav.Link href="/project" as={<Projects/>}>Projects</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
