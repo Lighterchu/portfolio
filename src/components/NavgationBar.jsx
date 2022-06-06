42a78f2362e751972234ab803161537ebe7a2119
Benjamin <benjaminrowe0@gmail.com>
2022-06-06 21:30:10 +1000

    still trying to fix the router

--- src/components/NavgationBar.jsx
+++ src/components/NavgationBar.jsx
@@ -15,7 +15,7 @@ function NavgationBar() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link href="/portfolio/about">About</Nav.Link>
       <Nav.Link href="/resume">Resume</Nav.Link>
       <Nav.Link href="/projects">Projects</Nav.Link>
     </Nav>
