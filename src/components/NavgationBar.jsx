40275c711cde301e469e6d9e7ee84c7318676603
Benjamin <benjaminrowe0@gmail.com>
2022-07-09 20:20:23 +1000

    fixed a miss spelled word

--- src/components/NavgationBar.jsx
+++ src/components/NavgationBar.jsx
@@ -19,18 +19,18 @@ export const NavgationBar = () =>  {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to={"portfoilo/about"}>About</Nav.Link>
-              <Nav.Link as={Link} to={"portfoilo/resume"}>Resume</Nav.Link>
-              <Nav.Link as={Link} to={"portfoilo/projects"}>Projects</Nav.Link>
+              <Nav.Link as={Link} to={"portfolio/about"}>About</Nav.Link>
+              <Nav.Link as={Link} to={"portfolio/resume"}>Resume</Nav.Link>
+              <Nav.Link as={Link} to={"portfolio/projects"}>Projects</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <div>
         <Routes>
-          <Route path="portfoilo/about" element={<About/>}></Route>
-          <Route path="portfoilo/resume" element={<Resume/>}></Route>
-          <Route path="portfoilo/projects" element={<Projects/>}></Route>
+          <Route path="portfolio/about" element={<About/>}></Route>
+          <Route path="portfolio/resume" element={<Resume/>}></Route>
+          <Route path="portfolio/projects" element={<Projects/>}></Route>
           <Route path="/*" element={<Home/>}></Route>
         </Routes>
     </div>
